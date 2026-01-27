c6a836f345102dd9f8b6bf11fc6d2c94c496ff50
Madeline Gyllenhoff <mgyllenhoff@gmail.com>
2026-01-26 19:39:47 -0500

    Adding original code

---
+++ build/CMakeFiles/Patterns.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mgyllenhoff/Library/CloudStorage/OneDrive-JohnsHopkins/Software Patterns/Assignments/Src_master/src/Attr.C" "CMakeFiles/Patterns.dir/Attr.C.o" "gcc" "CMakeFiles/Patterns.dir/Attr.C.o.d"
+  "/Users/mgyllenhoff/Library/CloudStorage/OneDrive-JohnsHopkins/Software Patterns/Assignments/Src_master/src/Document.C" "CMakeFiles/Patterns.dir/Document.C.o" "gcc" "CMakeFiles/Patterns.dir/Document.C.o.d"
+  "/Users/mgyllenhoff/Library/CloudStorage/OneDrive-JohnsHopkins/Software Patterns/Assignments/Src_master/src/Element.C" "CMakeFiles/Patterns.dir/Element.C.o" "gcc" "CMakeFiles/Patterns.dir/Element.C.o.d"
+  "/Users/mgyllenhoff/Library/CloudStorage/OneDrive-JohnsHopkins/Software Patterns/Assignments/Src_master/src/NamedNodeMap.C" "CMakeFiles/Patterns.dir/NamedNodeMap.C.o" "gcc" "CMakeFiles/Patterns.dir/NamedNodeMap.C.o.d"
+  "/Users/mgyllenhoff/Library/CloudStorage/OneDrive-JohnsHopkins/Software Patterns/Assignments/Src_master/src/Node.C" "CMakeFiles/Patterns.dir/Node.C.o" "gcc" "CMakeFiles/Patterns.dir/Node.C.o.d"
+  "/Users/mgyllenhoff/Library/CloudStorage/OneDrive-JohnsHopkins/Software Patterns/Assignments/Src_master/src/Test.C" "CMakeFiles/Patterns.dir/Test.C.o" "gcc" "CMakeFiles/Patterns.dir/Test.C.o.d"
+  "/Users/mgyllenhoff/Library/CloudStorage/OneDrive-JohnsHopkins/Software Patterns/Assignments/Src_master/src/Text.C" "CMakeFiles/Patterns.dir/Text.C.o" "gcc" "CMakeFiles/Patterns.dir/Text.C.o.d"
+  "/Users/mgyllenhoff/Library/CloudStorage/OneDrive-JohnsHopkins/Software Patterns/Assignments/Src_master/src/XMLSerializer.C" "CMakeFiles/Patterns.dir/XMLSerializer.C.o" "gcc" "CMakeFiles/Patterns.dir/XMLSerializer.C.o.d"
+  "/Users/mgyllenhoff/Library/CloudStorage/OneDrive-JohnsHopkins/Software Patterns/Assignments/Src_master/src/XMLTokenizer.C" "CMakeFiles/Patterns.dir/XMLTokenizer.C.o" "gcc" "CMakeFiles/Patterns.dir/XMLTokenizer.C.o.d"
+  "/Users/mgyllenhoff/Library/CloudStorage/OneDrive-JohnsHopkins/Software Patterns/Assignments/Src_master/src/XMLValidator.C" "CMakeFiles/Patterns.dir/XMLValidator.C.o" "gcc" "CMakeFiles/Patterns.dir/XMLValidator.C.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
